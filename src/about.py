ceb2f3c650621f506f56d11c810ad74c01b94fbe
Daryl Gan <80412090+darylGan@users.noreply.github.com>
2023-01-16 09:08:13 +0800

    Update about.py

--- src/about.py
+++ src/about.py
@@ -41,16 +41,16 @@ def app():
     with home_col_1:
         st.markdown("""
 
-        <h2 style="font-weight:bolder;font-size:20px;color:#216fdb;text-align:left;">What is Long Covid?</h2> 
+        <h2 style="font-weight:bolder;font-size:20px;color:#216fdb;text-align:left;">Malaysia’s Floods of December 2021: Can Future Disasters be Avoided?</h2> 
         
         """, unsafe_allow_html=True)
-        st.markdown("""For some people, coronavirus (COVID-19) can cause symptoms that last weeks or months after the infection has gone. This is sometimes called post-COVID-19 syndrome or '[long COVID](https://www.nhs.uk/conditions/coronavirus-covid-19/long-term-effects-of-coronavirus-long-covid/)'. COVID-19 survivors at all degrees of disease severity, including younger people, children, and those who are not hospitalized, are affected by this long COVID sickness, which is little understood. Many researchers have found that fatigue and difficulty breathing, which can last for months following exposure to COVID-19, are the most prevalent symptoms. Cognitive and mental impairments, chest and joint aches, palpitations, myalgia, smell and taste dysfunctions, cough, headache, and gastrointestinal and cardiac difficulties are all possible persistent symptoms of long COVID.
+        st.markdown("""The floods of December 2021 in Malaysia left almost 50 dead, required the evacuation of about 400,000 people, and resulted in an overall estimate of RM6.1 billion in financial losses. Unprecedented volumes of rainfall left areas on the west coast of Peninsular Malaysia under almost four meters of water and turned roads into rivers.
         """)
  
     with home_col_3:
         st.write("")
     # Video
-    st.markdown("Let's watch a video about Long-COVID recovery 🎥")
+    st.markdown("A video about the floods in 2021/2022")
 
     abt_col_1, abt_col_2, abt_col_3, abt_col_4,abt_col_5 = st.columns([0.1,2,1,1.5,1])
     with abt_col_1:
@@ -65,67 +65,50 @@ def app():
         st.write("")
     
     st.markdown("""
-
-    <h2 style="font-weight:bolder;font-size:20px;color:#216fdb;text-align:left;">What is this App about?</h2>
+    <h2 style="font-weight:bolder;font-size:20px;color:#216fdb;text-align:left;">Sentiment & Emotion Flood Detection through Twitter</h2>
     
-    This project aims to build a long COVID emotion analyzer. The application can analyze how people react to the long COVID and what are their emotions. The application can find out the trends of keywords used by people regarding the long COVID issues. The application can also analyze the emotion from the user input text. 
-
+    This project aims to build a sentiment & emotion analyzer about floods. The application can analyze how Malaysians react to the floods and what are their emotions. The application can also analyze the emotion from the user input text. 
     
-    <h2 style="font-weight:bolder;font-size:20px;color:#216fdb;text-align:left;">Who is this App for?</h2>
+    <h2 style="font-weight:bolder;font-size:20px;color:#216fdb;text-align:left;">Objectives</h2>
 
-    <p>Anyone can use this App completely for free! The target users are the people who are concerned about long COVID issues and long COVID patients. If you are interest in finding out how people feel about the pandemic and long COVID issues, this application would be a good choice for you to explore.
-    
-    If you like it ❤️, show your support by sharing 👍</p>
-    
-    <h2 style="font-weight:bolder;font-size:20px;color:#216fdb;text-align:left;">What is the features of this App?</h2>
+    1. To identify sentiment and emotion by using supervised machine learning algorithms.
+        - What algorithms can identify sentiment and emotion from tweets?
 
+    2.  To develop a sentiment and emotion-based flood detection model for English and Malay.
+        - How to develop a machine learning model for different languages? 
+        
+    3.  To evaluate the effectiveness of the flood detection model by using evaluation metrics.
+        - What metrics can evaluate the effectiveness of the machine learning model?
+
+    4. To build a sentiment and emotion-based flood detection dashboard.
+        - How can stakeholders be informed about the data insights and the use of the model?
+        
+    <h2 style="font-weight:bolder;font-size:20px;color:#216fdb;text-align:left;">Stakeholders</h2>
+
+    <p>Anyone can use this App completely for free! The target users include the general public who are concerned about floods, emergency response non-profit organizations and governmental bodies such as the Ministry Of Natural Resources, Environment And Climate Change.
+ 
+    <h2 style="font-weight:bolder;font-size:20px;color:#216fdb;text-align:left;">Features</h2>
+    
+    + Sentiment & Emotion Prediction
     + Data Exploration and Analysis
-    + Emotion Prediction
     + Monitoring Application
 
+    <h2 style="font-weight:bolder;font-size:20px;color:#216fdb;text-align:left;">Dataset</h2>
 
-    <h2 style="font-weight:bolder;font-size:20px;color:#216fdb;text-align:left;">What is the objectives of this App?</h2>
-
-    1. To explore the trend of keywords across the various timeline and different emotions
-        - What are the keyword trends for long COVID topics?
-            
-
-    2. To determine the emotions using supervised algorithms
-        - What machine learning algorithms can be used to determine the emotions from texts?   
-        
-        
-    3. To assess the effectiveness of the emotion model by using evaluation metrics
-        - What are metrics can be used to evaluate the emotion model?   
-
-
-    4. To develop a product for the emotion analyzer
-        - How to share the insights of the data and the application of the emotion model to stakeholders?   
-
-
-    <h2 style="font-weight:bolder;font-size:20px;color:#216fdb;text-align:left;">Data Information</h2>
-
-    The dataset to be used in this project is scraped from Twitter using Twitter API.     
+    The dataset used in this project is scraped from Twitter using Twitter API.     
     To access the dataset:   
-    https://drive.google.com/drive/folders/1cT9FzTWmjATdy7rlkrcRj_NU2zKCtYZE?usp=sharing
+    https://drive.google.com/drive/folders/...
 
     <h2 style="font-weight:bolder;font-size:20px;color:#216fdb;text-align:left;">Source Code</h2>
 
-    https://github.com/ganjoohan/Long-Covid-Emotion-Analyzer
-
-    <h2 style="font-weight:bolder;font-size:20px;color:#216fdb;text-align:left;">About me</h2>
-    I'm originally from Malacca, Malaysia, currenlty a third-year Bachelor of CS Data Science student at the University of Malaya!   
+    https://github.com/darylGan/Emocean
+    
+    <h2 style="font-weight:bolder;font-size:20px;color:#216fdb;text-align:left;">About the Developer</h2>
+    3rd year Bachelor of Computer Science (Data Science) student at University of Malaya   
 
     <br />
     <br />
 
-    ###### Made in [![this is an image link](https://i.imgur.com/iIOA6kU.png)](https://www.streamlit.io/)&nbsp, with ❤️ by [@JooHan](https://joohan.soho68.com/) &nbsp | &nbsp [![GitHub followers](https://img.shields.io/github/followers/ganjoohan?label=Github&style=social)](https://github.com/ganjoohan) &nbsp | &nbsp [![this is an image link](https://camo.githubusercontent.com/d7b9f7e3f8af9348678c5042440844da48b892fb320482f313d28366d10c25d5/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4c696e6b6564496e2d2532333030373742352e7376673f267374796c653d666f722d7468652d6261646765266c6f676f3d6c696e6b6564696e266c6f676f436f6c6f723d7768697465)](https://www.linkedin.com/in/gan-j-919226136/)
-
-
-
+    ###### Made in [![this is an image link](https://image.pngaaa.com/798/5084798-middle.png)](https://www.streamlit.io/) &nbsp, by Daryl Gan &nbsp | &nbsp [![this is an image link](https://allvectorlogo.com/img/2021/12/github-logo-vector.png)](https://github.com/darylGan) &nbsp | &nbsp [![this is an image link](https://www.citypng.com/public/uploads/preview/hd-linkedin-official-logo-transparent-background-31623962207jz85kqlqot.png)](https://www.linkedin.com/in/daryl-gan-/)
 
     """, unsafe_allow_html=True)
-
-
-
-
-    
