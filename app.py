9b059eaa80c60feaa7288258e740cfa2768a2f89
Daryl Gan <80412090+darylGan@users.noreply.github.com>
2023-01-19 23:11:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ MENU = {
 
 def main():
     menu_data = [
-        {'icon': "bi bi-houses-fill", 'label':"Rumah"},
+        {'icon': "bi bi-house-fill", 'label':"Rumah"},
         {'icon': "bi bi-bar-chart-line-fill", 'label':"Explore"},
         {'icon': "bi bi-tv-fill",'label':"Monitor"},
         {'icon': "bi bi-file-earmark-text-fill", 'label':"User Manual"},
