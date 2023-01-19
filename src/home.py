9357584035ebd73c4e6a5aebedff8bd5636a669a
Daryl Gan <80412090+darylGan@users.noreply.github.com>
2023-01-19 23:38:07 +0800

    Update home.py

--- src/home.py
+++ src/home.py
@@ -84,8 +84,6 @@ def get_prediction_proba(docx):
 #English Emotion Analyzer
 
 def app():
-    st.markdown(f'<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">', unsafe_allow_html=True)
-
     col_1, col_2, col_3 = st.columns([1,8,1])
 
     with col_1:
