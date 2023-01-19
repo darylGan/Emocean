08ae304d6e559ff2bb0566f789a49408a8ae3add
Daryl Gan <80412090+darylGan@users.noreply.github.com>
2023-01-20 04:33:48 +0800

    Update display.py

--- utils/display.py
+++ utils/display.py
@@ -6,7 +6,12 @@ def render_page(menupage):
     menupage.app()
 
 def title_awesome(body: str):
-    st.write()
+    st.write(
+        f"## Demo:  {body} "
+        "[![Quantum](https://cdn.rawgit.com/sindresorhus/awesome/"
+        "d7305f38d29fed78fa85652e3a63e154dd8e8829/media/badge.svg)]"
+        "(https://github.com/Avkash/mldl)"
+    )
 
 def render_md(md_file_name):
     st.markdown(get_file_content_as_string(md_file_name))
