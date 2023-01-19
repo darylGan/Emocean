7c59b8743635c3e2104a0ff5c64abccb7e50d8cb
Daryl Gan <80412090+darylGan@users.noreply.github.com>
2023-01-20 04:26:52 +0800

    Update track_utils.py

--- track_utils.py
+++ track_utils.py
@@ -1,24 +1,7 @@
-# Load Database Pkg
 import sqlite3
 conn = sqlite3.connect('data.db', check_same_thread=False)
 c = conn.cursor()
 
-
-# Fxn
-# def create_page_visited_table():
-# 	c.execute('CREATE TABLE IF NOT EXISTS pageTrackTable(pagename TEXT,timeOfvisit TIMESTAMP)')
-
-# def add_page_visited_details(pagename,timeOfvisit):
-# 	c.execute('INSERT INTO pageTrackTable(pagename,timeOfvisit) VALUES(?,?)',(pagename,timeOfvisit))
-# 	conn.commit()
-
-# def view_all_page_visited_details():
-# 	c.execute('SELECT * FROM pageTrackTable')
-# 	data = c.fetchall()
-# 	return data
-
-
-# Fxn To Track Input & Prediction
 def create_emotionclf_table():
 	c.execute('CREATE TABLE IF NOT EXISTS emotionclfTable(rawtext TEXT,prediction TEXT,probability NUMBER,timeOfvisit TIMESTAMP)')
 
@@ -29,4 +12,4 @@ def add_prediction_details(rawtext,prediction,probability,timeOfvisit):
 def view_all_prediction_details():
 	c.execute('SELECT * FROM emotionclfTable ORDER BY timeOfvisit DESC')
 	data = c.fetchall()
-	return 
+	return
