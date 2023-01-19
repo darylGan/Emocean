0325ee82c334e01df66db06bd6d4621cd9e20cef
Daryl Gan <80412090+darylGan@users.noreply.github.com>
2023-01-20 03:39:08 +0800

    Update track_utils.py

--- track_utils.py
+++ track_utils.py
@@ -3,10 +3,10 @@ conn = sqlite3.connect('data.db', check_same_thread=False)
 c = conn.cursor()
 
 def create_emotionclf_table():
-	c.execute('CREATE TABLE IF NOT EXISTS emotionclfTable(Input_Text TEXT,Emotion TEXT,Score NUMBER,Time_of_Visit TIMESTAMP)')
+	c.execute('CREATE TABLE IF NOT EXISTS emotionclfTable(rawtext TEXT,prediction TEXT,probability NUMBER,timeOfvisit TIMESTAMP)')
 
-def add_prediction_details(Input_Text,Emotion,Score,Time_of_Visit):
-	c.execute('INSERT INTO emotionclfTable(Input_Text,Emotion,Score,Time_of_Visit) VALUES(?,?,?,?)',(Input_Text,Emotion,Score,Time_of_Visit))
+def add_prediction_details(rawtext,prediction,probability,timeOfvisit):
+	c.execute('INSERT INTO emotionclfTable(rawtext,prediction,probability,timeOfvisit) VALUES(?,?,?,?)',(rawtext,prediction,probability,timeOfvisit))
 	conn.commit()
 
 def view_all_prediction_details():
